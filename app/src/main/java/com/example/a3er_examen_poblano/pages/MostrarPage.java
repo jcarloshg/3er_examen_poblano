fbbb0ea749bae9fc5f174a3530f2532c3eb33d34
jcarloshg <carlosj12336@gmail.com>
2021-11-18 08:45:24 -0600

    ADD pages

---
+++ app/src/main/java/com/example/a3er_examen_poblano/pages/MostrarPage.java
@@ -0,0 +1,14 @@
+package com.example.a3er_examen_poblano.pages;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.a3er_examen_poblano.R;
+
+public class MostrarPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mostrar_page);
+    }
+}
