8837f9c621f3b838766fbf4a7a6f5fd61929bc05
jcarloshg <carlosj12336@gmail.com>
2021-11-18 09:49:49 -0600

    implement method volley on MostrarPage.java

--- app/src/main/java/com/example/a3er_examen_poblano/pages/AgregarPage.java
+++ app/src/main/java/com/example/a3er_examen_poblano/pages/AgregarPage.java
@@ -2,13 +2,49 @@ package com.example.a3er_examen_poblano.pages;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.example.a3er_examen_poblano.R;
 
-public class AgregarPage extends AppCompatActivity {
+import org.json.JSONObject;
+
+public class AgregarPage extends AppCompatActivity implements Response.Listener<JSONObject>, Response.ErrorListener{
+
+    EditText editText_id, editText_nombre, editText_costo, editText_foto;
+    Button btn_agregar;
+
+    RequestQueue request;
+    JsonObjectRequest jsonObjectRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_page);
+
+        init_components();
+    }
+
+    private void init_components() {
+        editText_id = findViewById(R.id.editText_id);
+        editText_nombre = findViewById(R.id.editText_nombre);
+        editText_costo = findViewById(R.id.editText_costo);
+        editText_foto = findViewById(R.id.editText_foto);
+
+        btn_agregar = findViewById(R.id.btn_agregar);
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+
+    }
+
+    @Override
+    public void onResponse(JSONObject response) {
+
     }
 }
--- app/src/main/java/com/example/a3er_examen_poblano/pages/MostrarPage.java
+++ app/src/main/java/com/example/a3er_examen_poblano/pages/MostrarPage.java
@@ -1,29 +1,80 @@
 package com.example.a3er_examen_poblano.pages;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
+import android.app.VoiceInteractor;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.a3er_examen_poblano.R;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 import com.android.volley.Response;
 
 public class MostrarPage extends AppCompatActivity implements Response.Listener<JSONObject>, Response.ErrorListener{
 
+    EditText editText_info;
+    RequestQueue request;
+    JsonObjectRequest jsonObjectRequest;
+    final String URL = "http://serviciosdigitalesplus.com/catalogo.php";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mostrar_page);
+
+        init_components();
     }
+
 
     @Override
     public void onErrorResponse(VolleyError error) {
-        
+        Toast.makeText(this,""+error.toString(), Toast.LENGTH_SHORT).show();
+        Log.e("[onErrorResponse]" , "onErrorResponse: " + error.toString() );
+
     }
 
     @Override
     public void onResponse(JSONObject response) {
+        Toast.makeText(this,""+response.toString(), Toast.LENGTH_SHORT).show();
+        JSONArray jsonArray = response.optJSONArray("datos");
+        JSONObject jsonObject = null;
+        String line = "";
 
+        try {
+            jsonObject = jsonArray.getJSONObject(0);
+            line += "id: " + jsonObject.optString("id") + "\n";
+            line += "nombre: " + jsonObject.optString("nom") + "\n";
+            line += "cost: " + jsonObject.optString("costo") + "\n";
+            line += "foto: " + jsonObject.optString("foto") + "\n";
+
+            line += jsonArray.toString();
+
+            editText_info.setText(line);
+
+        }catch (Exception ex){
+            Log.e("[onResponse ] ", "onResponse: " + ex.toString() );
+        }
+
+        // editText_info.setText( jsonArray.toString() );
+    }
+
+    // ==============================================================
+    // ==============================================================
+    // ==============================================================
+    private void init_components() {
+        editText_info = findViewById(R.id.editText_info);
+
+        request = Volley.newRequestQueue(getApplicationContext());
+        jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, URL, null, this, this);
+        request.add(jsonObjectRequest);
     }
 }
